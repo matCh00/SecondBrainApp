9fb205db2902dde53856b99e879ad41cbb3c18e1
mateusz.chalik <mateusz.chalik@asseco.pl>
2023-04-20 15:27:20 +0200

    wyszukiwanie notatek

--- secondbrain/src/pages/NotesPage/NotesPage.tsx
+++ secondbrain/src/pages/NotesPage/NotesPage.tsx
@@ -6,14 +6,17 @@ import { types } from '../../models/Types';
 import { INoteGroup } from '../../models/INote';
 import { Card } from 'primereact/card';
 import Editor from "@monaco-editor/react";
-import { Fieldset } from 'primereact/fieldset';
+import { InputText } from 'primereact/inputtext';
 
 const NotesPage = () => {
 
   const [notes, setNotes] = useState<INoteGroup[]>([]);
   const [notesFiltered, setNotesFiltered] = useState<INoteGroup[]>([]);
 
+  const [searchValue, setSearchValue] = useState<string>('');
+
   let { id } = useParams();
+
 
   /** po każdej zmianie params */
   useEffect (() => {
@@ -21,28 +24,40 @@ const NotesPage = () => {
       (res) => {
         setNotes(res);  
         setNotesFiltered(res); 
-        console.log(res); 
       }
     )
   }, [id]);
 
+
+  /** filtracja */
+  const handleSearch = (e: string) => {
+    if (!e || e.length < 1) {
+      setNotesFiltered(notes);
+      return;
+    }
+    let tempNotes = notes.filter(n => n.name.toLowerCase().includes(e.toLowerCase()));
+    setNotesFiltered(tempNotes);
+  }
+
+
   return (
     <div className='p-5'>
+      <InputText onChange={(e) => handleSearch(e.target.value)} placeholder='Find' className='w-full'/>
+
       {notesFiltered.map(n => {
         return (
-          <Card title={n.name} key={n.name} className='mb-5'>
+          <Card title={n.name} key={n.name} className='mt-5'>
 
               {n.codes.map((i, index) => {
                 return (
 
-                  <Fieldset legend={i.language} key={i.code + index} className='mb-3'>
-                    <Editor
-                      language={i.language}
-                      value={i.code}
-                      theme='vs-dark'
-                      className='resize'
-                    />
-                  </Fieldset>
+                  <Editor
+                    key={i.code + index}
+                    language={i.language}
+                    value={i.code}
+                    theme='vs-dark'
+                    className='resize mb-3'
+                  />
                 )
               })}
           </Card>
